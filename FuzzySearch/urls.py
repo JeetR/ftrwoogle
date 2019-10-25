de4c38282793f6e34dddc57b6e37d64d40a998f0
satyajeet ranjan <ranjan.satyajeet2012@gmail.com>
2019-10-25 21:07:36 +0530

    Changed urls.py in root to open search page by default

--- FuzzySearch/urls.py
+++ FuzzySearch/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("search/",include('SearchApp.urls'))
+    path("search/",include('SearchApp.urls')),
+    path("",include('SearchApp.urls'))
 ]
